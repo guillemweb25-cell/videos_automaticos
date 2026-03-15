a58a23ae5925e02d30cd7f0231a5b37fb13b8a1f
Guillem <enguillem@gmail.com>
2026-03-15 19:56:48 +0100

    fix: remove outdated DOCKER_API_VERSION from start script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# Fix for "client version 1.53 is too new. Maximum supported API version is 1.41"
-export DOCKER_API_VERSION=1.41
-
-echo "Starting Docker services with DOCKER_API_VERSION=1.41..."
+echo "Starting Docker services..."
 #docker compose up -d --build
+
 docker compose up -d
